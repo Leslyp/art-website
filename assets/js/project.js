d3625992fdfaf2e0225bb2c54f03a898827d8708
Lesly <lperez1@redventures.com>
2018-03-12 11:17:35 -0400

    first commit

---
+++ assets/js/project.js
@@ -0,0 +1,15 @@
+jQuery( document ).ready( function( $ ) {
+	$( document ).foundation();
+	$(".mobile-nav__list-item--menu").on("click", function() {
+		$(".dropdown").toggle();
+	})
+
+	$(".media li").on("click", function(e) {
+		// e.preventDefault();
+		
+		$(this).find("a").addClass("selected-media");
+	})
+
+ });
+
+
